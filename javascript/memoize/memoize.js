ef3b897544dc0cee5d2e5a9e3cab1d88129d8bdf
LMuchapirei <h170407h@hit.ac.zw>
2023-04-24 09:36:36 +0200

    Refactor and add q2 memoize a function

---
+++ javascript/memoize/memoize.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    const memo ={}
+    return (...args) => 
+    memo[args.join()] ?? 
+    (memo[args.join()] = fn(...args)); 
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
+
