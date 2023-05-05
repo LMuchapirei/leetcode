cce75e95a9de76d9b0642608e703e86979e3eaeb
LMuchapirei <h170407h@hit.ac.zw>
2023-05-05 12:17:20 +0200

    bonus checkin: Sign of the product of an array

---
+++ python/sign_of_the_product_of_array/sign_of_the_product_of_array.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def arraySign(self,nums:List[int])->int:
+        neg=0
+        for n in nums:
+            if n ==0:
+                return 0
+            neg += (1 if n < 0 else 0)
+        return 1 if neg %2 ==0 else -1
