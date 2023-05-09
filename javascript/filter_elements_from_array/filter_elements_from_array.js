75f41ea508a74b1ee0e0e0912160751b704f6fb3
LMuchapirei <h170407h@hit.ac.zw>
2023-05-09 12:44:41 +0200

    daily-checkin: 30 days js

---
+++ javascript/filter_elements_from_array/filter_elements_from_array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    const res = []
+    for(let idx=0;idx<arr.length;idx++){
+        if(fn(arr[idx],idx))
+            res.push(arr[idx])
+    }
+    return res
+    };
