d96063318de6dec7822183fd7571e4647d33703f
LMuchapirei <muchapireil@gmail.com>
2024-06-21 10:38:14 +0200

    Revising yesterday question

--- python/merge-sorted-list/merge-two-sorted-lists.py
+++ python/merge-sorted-list/merge-two-sorted-lists.py
@@ -23,3 +23,28 @@ class Solution:
             tail.next = l2
 
         return dummy.next # dummy points to ListNode() first and the next node is what we want to return
+    
+
+    # Revision hha ha 
+    def mergeSortedList(l1,l2):
+        dummy = ListNode()
+        tail = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+
+            else:
+                tail.next = l2
+                tail.next = l2
+                l2 = l2.next
+            
+            tail = tail.next
+
+        if l2:
+            tail.next = l2
+        elif l1:
+            tail.next = l1
+
+        return dummy.next
