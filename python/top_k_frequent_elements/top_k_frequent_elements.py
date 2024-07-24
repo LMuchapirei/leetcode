b4bcf9a73603e58b90c2e1477dcc0b821200ee7c
LMuchapirei <muchapireil@gmail.com>
2024-07-24 08:43:19 +0200

    Commentary added to the k most frequent elements

--- python/top_k_frequent_elements/top_k_frequent_elements.py
+++ python/top_k_frequent_elements/top_k_frequent_elements.py
@@ -37,3 +37,25 @@ class Solution:
                 res.append(n)
                 if len(res)==k:
                     return res
+
+# solution with some commentary         
+# class Solution:  
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         count = {}
+#         freq = [[] for i in range(len(nums)+1)]
+#         res = []
+#         #count the elements up
+#         for n in nums:
+#             count[n] = count.get(n,0) + 1
+#         #put the elements in appropriate bucket count (count is 2 put it in the buckct where count is 2)
+#         for n,c in count.items():
+#             freq[c].append(n)
+#         # from the end of the freqeuncy list
+#         for i in range(len(freq)-1,0,-1):
+#             # get all element in a given bucket(match a certaion count that is at location i in the frequency array)
+#             for n in freq[i]:
+#                 # append them to the result
+#                 res.append(n)
+#                 # check we have reached the count of elements we are looking for return if so correct
+#                 if len(res)==k:
+#                     return res
